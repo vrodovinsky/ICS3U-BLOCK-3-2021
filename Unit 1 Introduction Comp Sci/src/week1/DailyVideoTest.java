067670947ad9b696254e12894f31f46dd4a67e58
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2021-09-09 15:35:47 -0400

    Daily Video 1 Test

---
+++ Unit 1 Introduction Comp Sci/src/week1/DailyVideoTest.java
@@ -0,0 +1,17 @@
+package week1;
+
+public class DailyVideoTest {
+    public static void main(String[] args) {
+        System.out.print("AP ");
+        System.out.print("CS ");
+        System.out.print("A ");
+        System.out.println("Rocks!");
+
+        System.out.print("This ");
+        System.out.print("is a ");
+        System.out.print("test. ");
+        System.out.println("AP CS");
+        System.out.println("A Rocks");
+        System.out.print("!!");
+    }
+}
