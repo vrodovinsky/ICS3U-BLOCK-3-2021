e71c715e7119d74e82f3fafe5fc91a5df2a779fd
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2021-09-09 13:15:20 -0400

    Unit 1 initial commit

---
+++ Unit 1 Introduction Comp Sci/src/week1/ExampleOne.java
@@ -0,0 +1,12 @@
+package week1;
+
+
+public class ExampleOne {
+
+    public static void main(String[] args) {
+      System.out.println("Welcome to ICS3U - AP");  // System.out.println displays text to the screen and hits enter
+    }
+
+    
+    
+}
---
+++ Unit 1 Introduction Comp Sci/src/week1/ExampleTwo.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class ExampleTwo {
+  public static void main(String[] args) {
+      System.out.println("A"); // prints A and hits enter
+      System.out.print("B"); // prints B
+      System.out.print("C"); // prints C
+      System.out.println("D"); //prints D and hits enter
+      System.out.println("E"); // prints E and hits enter
+
+
+  }  
+}
