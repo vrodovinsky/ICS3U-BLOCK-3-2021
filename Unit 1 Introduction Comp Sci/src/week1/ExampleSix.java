6389b7630cee998dacf6d7819fe9b7e6a9a3aae4
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2021-09-13 12:59:27 -0400

    The difference between x++ and ++x

--- Unit 1 Introduction Comp Sci/src/week1/ExampleSix.java
+++ Unit 1 Introduction Comp Sci/src/week1/ExampleSix.java
@@ -6,24 +6,36 @@ public class ExampleSix {
         int y = 3;
         double z = 1;
 
-        x = x + 1;  // tkae x add 1 to it and assign it back to x
-        y = y - 1;  // (y = 2)
-        x = x+ 3;   // (x = 5)
+        x = x + 1; // take x add 1 to it and assign it back to x
+        y = y - 1; // (y = 2)
+        x = x + 3; // (x = 5)
 
         x += 1; // equvilent to x = x + 1;
         y -= 1; // equvielent to y = y - 1;
-        x += 3;  // equvilent to x = x+ 3;
+        x += 3; // equvilent to x = x+ 3;
 
-        y = y / 2;  // equvilent to y /= 2;
+        y = y / 2; // equvilent to y /= 2;
         z = z * 2; // equvilent to z *= 2;
 
-        x++;    // increment by 1
-        y--;    // decrease by 1
+        x++; // increment by 1
+        y--; // decrease by 1
 
         // modulus operator % -> returns the remainder
 
-        System.out.println(13 % 3);   // 13/3 what is the remainder
-        y = y % 2;  // equvilent to y %= 2;
+        System.out.println(13 % 3); // 13/3 what is the remainder
+        y = y % 2; // equvilent to y %= 2;
+
+        int a = 5;
+        a++;
+        ++a;
+        a--;
+        --a;
+
+        int b = 3;
+        int e = 3;
+
+        int c = 2 * b++;
+        int d = 2 * ++e;
 
     }
 }
