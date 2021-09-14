c8ba5c44a112491d319203ddc8b2e862078fe216
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2021-09-14 13:57:03 -0400

    Test Commit 2

--- Unit 1 Introduction Comp Sci/src/week1/ExampleOne.java
+++ Unit 1 Introduction Comp Sci/src/week1/ExampleOne.java
@@ -4,6 +4,7 @@ public class ExampleOne {
 
   public static void main(String[] args) {
     System.out.println("Welcome to ICS3U - AP!"); // System.out.println displays text to the screen and hits enter
+    System.out.println("Welcome to ICS3U - AP!");
 
     // Anything in quotes is considered a String Literal
     // A String is a sequence of characters (Strings are in double quotes)
