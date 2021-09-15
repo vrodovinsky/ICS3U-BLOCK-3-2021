8659d74ef3575e0e11a8c8760016d5e0e51309e8
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2021-09-15 16:44:06 -0400

    Student Example

---
+++ Unit 2 - Using Objects/src/week2/StudentExample.java
@@ -0,0 +1,9 @@
+package week2;
+
+public class StudentExample {
+    public static void main(String[] args) {
+        // ritchie is a variable to an instance of the class student
+        // ritchie is an Object (instances of a class)
+        Student ritchie = new Student("Ritchie", "45646", 12);
+    }
+}
