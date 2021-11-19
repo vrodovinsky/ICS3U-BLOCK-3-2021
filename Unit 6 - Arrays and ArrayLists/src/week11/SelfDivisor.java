7a686013338fe73dceb1ef8fafd3dd61b872507e
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2021-11-19 13:14:52 -0500

    Unit 6 FRQ Practice

---
+++ Unit 6 - Arrays and ArrayLists/src/week11/SelfDivisor.java
@@ -0,0 +1,30 @@
+package week11;
+
+public class SelfDivisor {
+
+    public static boolean isSelfDivisor(int num) {
+        String temp = "" + num;
+        for (int i = 0; i < temp.length(); i++) {
+            int digit = Integer.parseInt(temp.substring(i, i + 1));
+            if (num / digit != 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static int[] firstNumSelfDivisors(int start, int num) {
+        int[] temp = new int[num];
+
+        int count = 0;
+        while (count < temp.length) {
+            if (isSelfDivisor(start)) {
+                temp[i] = start;
+                count++;
+            }
+            start++;
+
+        }
+        return temp;
+    }
+}
