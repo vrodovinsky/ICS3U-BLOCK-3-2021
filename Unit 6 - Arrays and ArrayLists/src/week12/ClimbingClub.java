0d2b2dcaee4daecb58c8fb7fb2bab9568434480d
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2021-12-02 13:01:55 -0500

    ArrayList FRQ Practice

---
+++ Unit 6 - Arrays and ArrayLists/src/week12/ClimbingClub.java
@@ -0,0 +1,21 @@
+package week12;
+
+import java.util.ArrayList;
+
+public class ClimbingClub {
+    /*
+     * private List<ClimbInfo> climbList;
+     * 
+     * public void addClimb(String peakName, int climbTime) {
+     * climbList.add(new ClimbInfo(peakName, climbTime));
+     * 
+     * for (int i = 0; i < climbList.size(); i++) {
+     * String t = climbList.get(i).getPeakName();
+     * if (peakName.compareTo(t) < 0)
+     * climbList.add(i, new ClimbInfo(peakName, climbTime));
+     * return;
+     * }
+     * climbList.add(new ClimbInfo(peakName, climbTime));
+     * }
+     */
+}
---
+++ Unit 6 - Arrays and ArrayLists/src/week12/MusicDownloads.java
@@ -0,0 +1,27 @@
+package week12;
+
+public class MusicDownloads {
+    /*
+     * private List<DownloadInfo> downloadList;
+     * 
+     * public DownloadInfo getDownloadInfo(String title) {
+     * for (DownloadInfo di : downloadList) {
+     * if (title.equals(di.getTitle()))
+     * return di;
+     * }
+     * return null;
+     * }
+     * 
+     * public void updateDownloads(List<String> titles) {
+     * for (String title : titles {
+     * DownloadInfo di = getDownloadInfo(title);
+     * if (di == null){
+     * di = new DownloadInfo(title);
+     * downloadInfoList.add(di);
+     * }
+     * di.incrementTimesDownloaded();
+     * }
+     * 
+     * }
+     */
+}
