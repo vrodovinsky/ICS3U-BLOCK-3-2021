5bfeda1ebeae457193fa4670496fac2aa35e4e8a
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2021-12-03 12:42:42 -0500

    ArrayList frq practice

---
+++ Unit 6 - Arrays and ArrayLists/src/week12/MasterOrder.java
@@ -0,0 +1,29 @@
+package week12;
+
+public class MasterOrder {
+    /*
+     * private List<CookieOrder> orders;
+     * 
+     * public int getTotalBoxes() {
+     * int total = 0;
+     * 
+     * for (CookieOrder box : orders) {
+     * total += box.getNumBoxes();
+     * }
+     * 
+     * return total;
+     * }
+     * 
+     * public int removeVariety(String cookieVar) {
+     * int total = 0;
+     * for (int i = 0; i < orders.size(); i++) {
+     * if (cookieVar.equals(orders.get(i).getVariety())) {
+     * total+= orders.get(i).getNumBoxes();
+     * orders.remove(i);
+     * i--;
+     * }
+     * }
+     * return total;
+     * }
+     */
+}
