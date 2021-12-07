085e6c3ba99481639dc029c780de4071ff57d201
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2021-12-07 12:41:09 -0500

    frq practice

---
+++ Unit 6 - Arrays and ArrayLists/src/week12/OnlinePurchaseManager.java
@@ -0,0 +1,29 @@
+package week12;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+import week11.ForEachExample;
+
+public class OnlinePurchaseManager {
+    private ArrayList<Gizmo> purchases;
+
+    public int countElectronicsByMaker(String maker) {
+        int count = 0;
+        for (Gizmo g : purchases) {
+            if (maker.equals(g.getMaker()) && g.isElectronic())
+                count++;
+        }
+    }
+
+    public boolean hasAdjacentEqualPair() {
+        if (purchases.size() < 2)
+            return false;
+
+        for (int i = 0; i < purchases.size() - 1; i++) {
+            if (purchases.get(i).equals(purchases.get(i + 1)))
+                return true;
+        }
+        return false;
+    }
+
+}
