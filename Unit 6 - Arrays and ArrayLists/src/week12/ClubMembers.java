01d2bce6b2233e499f0800209820cf84da34a43f
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2021-12-07 13:25:21 -0500

    frq arraylist practice

---
+++ Unit 6 - Arrays and ArrayLists/src/week12/ClubMembers.java
@@ -0,0 +1,26 @@
+package week12;
+
+public class ClubMembers {
+    /*
+     * private ArrayList<MemberInfo> memberList;
+     * 
+     * public void addMembers(String[] names, int gradYear) {
+     * for (String name : names) {
+     * MemberInfo mi = new MemberInfo(name, gradYear);
+     * memberList.add(mi);
+     * }
+     * }
+     * 
+     * public ArrayList<MemberInfo> removeMembers(int year) {
+     * ArrayList<MemberInfo> goodStanding = new ArrayList<MemberInfo>();
+     * for (int i = memberList.size(); i >= 0; i--) {
+     * if (memberList.get(i).getGradYear() <= year)
+     * memberList.remove(i);
+     * if (memberList.get(i).getGradYear() <= year &&
+     * memberList.get(i).inGoodStanding() == true)
+     * goodStanding.add(memberlist.get(i));
+     * }
+     * return goodStanding;
+     * }
+     */
+}
---
+++ Unit 6 - Arrays and ArrayLists/src/week12/Delimiters.java
@@ -0,0 +1,34 @@
+package week12;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+public class Delimiters {
+    /*
+     * private String openDel;
+     * private String closeDel;
+     * ArrayList<String> delimiters = new ArrayList<String>();
+     * 
+     * public ArrayList<String> getDelimitersList(String[] tokens) {
+     * for (String d : tokens) {
+     * if (d.equals(openDel) || d.equals(closeDel))
+     * delimeters.add(d);
+     * }
+     * return delimeters;
+     * }
+     * 
+     * public boolean isBalanced(ArrayList<String> delimiters) {
+     * int isOpen = 0;
+     * int isClosed = 0;
+     * 
+     * for (String d : delimiters) {
+     * if (d.equals(openDel))
+     * isOpen++;
+     * if (d.equals(closeDel))
+     * isClosed++;
+     * if (isOpen < isClosed)
+     * return false;
+     * }
+     * return isOpen == isClosed;
+     * }
+     */
+}
