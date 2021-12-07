1dceb2667d1035f4d898f6daf8f0d376bf41920b
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2021-12-07 12:41:44 -0500

    practice

--- Unit 6 - Arrays and ArrayLists/src/week12/OnlinePurchaseManager.java
+++ Unit 6 - Arrays and ArrayLists/src/week12/OnlinePurchaseManager.java
@@ -5,25 +5,26 @@ import javax.swing.text.StyledEditorKit.ForegroundAction;
 import week11.ForEachExample;
 
 public class OnlinePurchaseManager {
-    private ArrayList<Gizmo> purchases;
-
-    public int countElectronicsByMaker(String maker) {
-        int count = 0;
-        for (Gizmo g : purchases) {
-            if (maker.equals(g.getMaker()) && g.isElectronic())
-                count++;
-        }
-    }
-
-    public boolean hasAdjacentEqualPair() {
-        if (purchases.size() < 2)
-            return false;
-
-        for (int i = 0; i < purchases.size() - 1; i++) {
-            if (purchases.get(i).equals(purchases.get(i + 1)))
-                return true;
-        }
-        return false;
-    }
-
+    /*
+     * private ArrayList<Gizmo> purchases;
+     * 
+     * public int countElectronicsByMaker(String maker) {
+     * int count = 0;
+     * for (Gizmo g : purchases) {
+     * if (maker.equals(g.getMaker()) && g.isElectronic())
+     * count++;
+     * }
+     * }
+     * 
+     * public boolean hasAdjacentEqualPair() {
+     * if (purchases.size() < 2)
+     * return false;
+     * 
+     * for (int i = 0; i < purchases.size() - 1; i++) {
+     * if (purchases.get(i).equals(purchases.get(i + 1)))
+     * return true;
+     * }
+     * return false;
+     * }
+     */
 }
